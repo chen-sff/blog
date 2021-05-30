672049779cac1b3fe185cde74452cb232d5adf88
cdw <1761669403@qq.com>
2021-05-30 20:54:48 +0800

    updata

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
+var MysqlPool = require('./db/mysqlPool');
+const mysqlPool = new MysqlPool();
+const pool = mysqlPool.getPool();
 
-/* GET home page. */
+
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+var selectSql = "select title,content from article order by title asc "
+pool.getConnection((err,connect)=>{
+  console.log(err);
+  connect.query(selectSql,(err,rusult)=>{
+    res.render('index',{ data:rusult });
+    });
+  });
 });
+
+
 
 module.exports = router;

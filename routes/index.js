f0c7fc2e568b2c34c5a61db51de77b41eb21b9f1
cdw <1761669403@qq.com>
2021-06-07 19:49:11 +0800

    updata

--- routes/index.js
+++ routes/index.js
@@ -14,17 +14,31 @@ pool.getConnection((err,connect)=>{
     });
   });
 });
-router.get('/search', function(req, res, next) {
+router.get('/ser', function(req, res, next) {
   var searchSql = "select id,title,content from article  "
   pool.getConnection((err,connect)=>{
     console.log(err);
     connect.query(searchSql,[req.query.id,req.query.title,req.query.content],(err,rusult)=>{
       console.log(req.query.keyword);
-      res.render('search',{ data:rusult[0] });
+      res.render('search',{ data:rusult[0]});
 
       });
     });
   });
+
+  router.get('/search', function(req, res, next) {
+    var searchSql = "select id,title,content from article where title like'%"+req.query.keyword+"%'"
+    pool.getConnection((err,connect)=>{
+      connect.query(searchSql,(err,rusult)=>{
+        if(err){
+          console.log(err);
+        }
+        
+        res.render('index',{ data:rusult});
+  
+        });
+      });
+    });
 
 
 
